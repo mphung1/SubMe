ef88bd02d655d29dc942ccd289d124ac2b9dcb9b
Minh Phung <mphung1@villanova.edu>
2022-12-30 18:30:58 -0500

    add deployment config

--- WebAppFrontend/src/pages/api/transcription.ts
+++ WebAppFrontend/src/pages/api/transcription.ts
@@ -2,5 +2,5 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://subme-api.ue.r.appspot.com/",
+  baseURL: "https://subme-373121.ue.r.appspot.com/",
 });
